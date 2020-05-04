12d9dc8a89e9604faff80918a5f503b94846502c
Ahmed <a.abdelmoula@adveez.com>
2020-05-04 09:35:13 +0100

    feat(blog): fix docker-compose exit code zero

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Navbar from "./Containers/nav/Navbar";
-import Header from "./Containers/header/Header";
-import Footer from "./Containers/footer/Footer";
-import { NotFound, SideBarLeft, Container } from "./Containers/pages";
-import { PAGES } from "./constants";
+import Navbar from './Containers/nav/Navbar';
+import Header from './Containers/header/Header';
+import Footer from './Containers/footer/Footer';
+import { NotFound, SideBarLeft, Container } from './Containers/pages';
+import { PAGES } from './constants';
 
-function App() {
-  const [active, setActive] = useState("Docker");
+function App () {
+  const [active, setActive] = useState('Docker')
   const renderContainer = () => {
-    let body = [];
-    if (active !== "Home") {
-      body.push(<header id="head" className="secondary" />);
+    let body = []
+    let hasBody = true
+    if (active !== 'Home') {
+      body.push(<header id='head' className='secondary' />)
     }
     switch (active) {
-      case "Home":
-        body.push(<Header />);
-        body.push(<Container />);
+      case 'Home':
+        body.push(<Header />)
+        body.push(<Container />)
         break;
-      case "Docker":
-        body.push(<SideBarLeft />);
+      case 'Docker':
+        body.push(<SideBarLeft />)
         break;
       default:
-        body.push(<NotFound />);
+        hasBody = false
+        body.push(<NotFound />)
         break;
     }
-    return body;
-  };
+    if (hasBody) { body.push(<Footer />) }
+    return body
+  }
 
   const handlerPageSelect = (name) => {
-    setActive(name);
+    setActive(name)
   };
 
   return PAGES.length ? (
-    <div className="home">
+    <div className='home'>
       <Navbar
         pages={PAGES}
         active={active}
         handlerPageSelect={handlerPageSelect}
       />
       {renderContainer()}
-      <Footer />
     </div>
   ) : (
     <h1>Loading....</h1>
-  );
+  )
 }
 
-export default App;
+export default App
